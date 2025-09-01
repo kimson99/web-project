8e99e7ec86fbcfeceb9df0e84863ca01e5628845
KS <sonngo81.99@gmail.com>
2025-09-01 13:52:14 +0700

    fix(input): placeholder color

--- packages/frontend/src/components/CustomInputField.tsx
+++ packages/frontend/src/components/CustomInputField.tsx
@@ -1,38 +1,38 @@
-import type { FieldValues, Path, UseFormRegister } from "react-hook-form";
+import type { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
 interface CustomInputFieldProps<T extends FieldValues> {
-	labelName: string;
-	fieldName: Path<T>;
-	type: React.HTMLInputTypeAttribute;
-	register: UseFormRegister<T>;
-	isRequired?: boolean;
-	errorMessage?: string;
-	placeholder?: string;
+  labelName: string;
+  fieldName: Path<T>;
+  type: React.HTMLInputTypeAttribute;
+  register: UseFormRegister<T>;
+  isRequired?: boolean;
+  errorMessage?: string;
+  placeholder?: string;
 }
 
 const CustomInputField = <T extends FieldValues>({
-	labelName,
-	fieldName,
-	errorMessage,
-	placeholder,
-	isRequired,
-	type,
-	register,
+  labelName,
+  fieldName,
+  errorMessage,
+  placeholder,
+  isRequired,
+  type,
+  register,
 }: CustomInputFieldProps<T>) => {
-	return (
-		<>
-			<label className="fieldset-label">
-				{`${labelName}${isRequired ? "*" : ""}`}
-			</label>
-			<input
-				type={type}
-				placeholder={placeholder}
-				className="input w-full"
-				{...register(fieldName)}
-			/>
-			{errorMessage && <div className="text-error">{errorMessage}</div>}
-		</>
-	);
+  return (
+    <>
+      <label className="fieldset-label">
+        {`${labelName}${isRequired ? '*' : ''}`}
+      </label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="input w-full placeholder:opacity-50"
+        {...register(fieldName)}
+      />
+      {errorMessage && <div className="text-error">{errorMessage}</div>}
+    </>
+  );
 };
 
 export default CustomInputField;
